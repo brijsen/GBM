1cfcda311073916e30f191c1f6de0fa7a13a5994
Saurabh.V <Saurabh.V@ril.com>
2022-03-11 21:30:24 +0530

    Initial commit

---
+++ src/main/java/com/wa/tool/GbmApplication.java
@@ -0,0 +1,13 @@
+package com.wa.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wa/tool/GbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wa.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
